b4072f6866f14b2c7ecedd962b8755603af845ea
adamsb0713 <briadams2016@gmail.com>
2018-04-18 14:44:18 -0400

    commit 2

--- Scripts/helper.R
+++ Scripts/helper.R
@@ -1,5 +1,5 @@
 {
   rm(list=ls());                         # clear Console Window
   options(show.error.locations = TRUE);  # show line numbers on error
-  library(ggplot2);                      # include all GGPlot2 functions
+  library(ggplot2);                      # include all GGPlot2 functions 
 }
